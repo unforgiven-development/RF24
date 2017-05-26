168cd93df517c04236b3c4d73d2331fcee27212b
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2017-05-26 12:45:02 -0500

    Fix a bad merge

--- utility/ATTiny/RF24_arch_config.h
+++ utility/ATTiny/RF24_arch_config.h
@@ -1,9 +1,4 @@
 /*
-<<<<<<< HEAD
- * TMRh20 2015
- * ATTiny Configuration File
-*/
-=======
  * RF24 -- utility/ATTiny/RF24_arch_config.h
  *
  * (c) 2016 Gerad Munsch <gmunsch@unforgivendevelopment.com>
@@ -11,50 +6,33 @@
  *
  * RF24 Configuration file for ATTiny devices, particularly the ATTiny 85/84
  */
->>>>>>> github/master
 
-#ifndef __RF24_ARCH_CONFIG_H__
-#define __RF24_ARCH_CONFIG_H__
+#ifndef _RF24_ARCH_CONFIG_H__
+#define _RF24_ARCH_CONFIG_H__
 
-<<<<<<< HEAD
-  /********** USER DEFINES: **********/  
-  //#define FAILURE_HANDLING
-  //#define MINIMAL
-  /***********************************/
-=======
-  /************************************/
-  /********** USER DEFINES:  **********/
-  /************************************/
-  //#define FAILURE_HANDLING
-  //#define MINIMAL
-  /************************************/
->>>>>>> github/master
+/************************************/
+/********** USER DEFINES:  **********/
+/************************************/
+//#define FAILURE_HANDLING
+//#define MINIMAL
+/************************************/
 
-  #define rf24_max(a,b) (a>b?a:b)
-  #define rf24_min(a,b) (a<b?a:b)
+#define rf24_max(a,b) (a>b?a:b)
+#define rf24_min(a,b) (a<b?a:b)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> github/master
-  #if ARDUINO < 100
-  	#include <WProgram.h>
-  #else
-  	#include <Arduino.h>
-  #endif
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
 
-  #include <stddef.h>  
-<<<<<<< HEAD
-=======
-  
+#include <stddef.h>
+
   /*
    * Include the header file for SPI functions
    * (Main SPI code is contained in RF24.cpp for simplicity)
    */
-  #include "spi.h"
->>>>>>> github/master
-
-  // Include the header file for SPI functions (Main SPI code is contained in RF24.cpp for simplicity)
   #include "spi.h"
   #define _SPI SPI
 
@@ -69,10 +47,6 @@
 
   #include <avr/pgmspace.h>
   #define PRIPSTR "%S"
-<<<<<<< HEAD
 
-#endif // __RF24_ARCH_CONFIG_H__
-=======
->>>>>>> github/master
 
-#endif // __RF24_ARCH_CONFIG_H__
+#endif	/* _RF24_ARCH_CONFIG_H__ */
--- utility/Due/RF24_arch_config.h
+++ utility/Due/RF24_arch_config.h
@@ -1,52 +1,40 @@
 /*
-<<<<<<< HEAD
- * TMRh20 2015
-=======
  * RF24 -- utility/Due/RF24_arch_config.h
  *
  * (c) 2016 Gerad Munsch <gmunsch@unforgivendevelopment.com>
  * (c) 2015 TMRh20
  *
->>>>>>> github/master
  * RF24 Configuration file for Arduino Due
  */
 
-#ifndef __RF24_ARCH_CONFIG_H__
-#define __RF24_ARCH_CONFIG_H__
+#ifndef _RF24_ARCH_CONFIG_H__
+#define _RF24_ARCH_CONFIG_H__
 
-<<<<<<< HEAD
-  /********** USER DEFINES: **********/
-  //#define FAILURE_HANDLING
-  //#define SERIAL_DEBUG
-  //#define MINIMAL
-  /***********************************/
-=======
-  /************************************/
-  /********** USER DEFINES:  **********/
-  /************************************/
-  //#define FAILURE_HANDLING
-  //#define SERIAL_DEBUG
-  //#define MINIMAL
-  /************************************/
->>>>>>> github/master
+/************************************/
+/********** USER DEFINES:  **********/
+/************************************/
+//#define FAILURE_HANDLING
+//#define SERIAL_DEBUG
+//#define MINIMAL
+/************************************/
 
-  #define rf24_max(a,b) (a>b?a:b)
-  #define rf24_min(a,b) (a<b?a:b)
+#define rf24_max(a,b) (a>b?a:b)
+#define rf24_min(a,b) (a<b?a:b)
 
-  #include <Arduino.h>
-  #include <SPI.h>
+#include <Arduino.h>
+#include <SPI.h>
 
-  #define _BV(x) (1<<(x))
-  #define _SPI SPI
+#define _BV(x) (1<<(x))
+#define _SPI SPI
 
-  #ifdef SERIAL_DEBUG
-    #define IF_SERIAL_DEBUG(x) ({x;})
-  #else
-    #define IF_SERIAL_DEBUG(x)
-  #endif
+#ifdef SERIAL_DEBUG
+#define IF_SERIAL_DEBUG(x) ({x;})
+#else
+#define IF_SERIAL_DEBUG(x)
+#endif
 
-  #define printf_P printf
-  #define strlen_P strlen
-  #define PRIPSTR "%s"
+#define printf_P printf
+#define strlen_P strlen
+#define PRIPSTR "%s"
 
-#endif // __RF24_CONFIG_H__
+#endif	/* _RF24_CONFIG_H__ */
